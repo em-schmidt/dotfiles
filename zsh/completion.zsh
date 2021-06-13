0ae082a8411505490a335beb72c938984f461a73
eric m. schmidt <eric@spaceba.by>
2021-06-13 18:35:04 -0400

    brew path support for both intel & m1

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,10 +1,9 @@
-fpath=(/usr/local/share/zsh/site-functions $fpath)
+fpath=(${BREWPATH}/share/zsh/site-functions $fpath)
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
-source /opt/homebrew/share/zsh/site-functions/aws_zsh_completer.sh
+source ${BREWPATH}/share/zsh/site-functions/aws_zsh_completer.sh
 
-complete -o nospace -C /usr/local/bin/vault vault
