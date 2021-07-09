bb1cb23c4f5e3ac133f3be0b486c275fed58af73
eric m. schmidt <eric.schmidt@kasisto.com>
2021-07-09 12:03:26 -0400

    homebrew installer fix and re-order for cleaner bootstrap

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -3,24 +3,10 @@
 # Homebrew
 #
 # This checks for a homebrew install and installs it if not found
-# 
 
-# Check for Homebrew
-if which brew
+if ! which brew > /dev/null
 then
-  echo "brew found at $(which brew)"
-else
   echo "  Installing Homebrew for you."
-
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-  fi
-
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-exit 0
