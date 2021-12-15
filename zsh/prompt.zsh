7e8e8417dce30da259c8d3ca98fcc938f1848fcb
eric m. schmidt <eric@spaceba.by>
2021-12-15 12:24:20 -0500

    use native vsc_info for zsh git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,9 +1,20 @@
 autoload colors && colors
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git*' formats " (%b|%c%u)"
+zstyle ':vcs_info:git*' actionformats " (%b|%a%c%u)"
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' unstagedstr '…'
+zstyle ':vcs_info:*' stagedstr '•'
+precmd() {
+  vcs_info
+}
 
-source "${BREWPATH}/opt/zsh-git-prompt/zshrc.sh"
+setopt prompt_subst
+
 [[ -e ${BREWPATH}/opt/kube-ps1/share/kube-ps1.sh ]] && source "${BREWPATH}/opt/kube-ps1/share/kube-ps1.sh"
 
-PROMPT='%B%m:%25<...<%~%<<%b$(git_super_status) %# '
+PROMPT='%B%m:%25<...<%~%<<%b${vcs_info_msg_0_} %# '
 
 function aws_profile {
   if [ -v AWS_PROFILE ]; then
