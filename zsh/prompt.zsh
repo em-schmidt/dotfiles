b98f6d60607b129724a1dccc620fe614f8b2258f
eric m. schmidt <eric@spaceba.by>
2022-01-28 21:10:54 -0500

    add untracked file count to git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,11 +1,24 @@
 autoload colors && colors
+
+
+# git status prompt bits
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:git*' formats " (%b|%c%u)"
 zstyle ':vcs_info:git*' actionformats " (%b|%a%c%u)"
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr '…'
+zstyle ':vcs_info:*' unstagedstr '✚'
 zstyle ':vcs_info:*' stagedstr '•'
+zstyle ':vcs_info:*+set-message:*' hooks git-untracked
+
++vi-git-untracked() {
+if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == 'true' ]] && \
+  git status --porcelain | grep -q '^?? ' 2>/dev/null ; then
+  untracked_count=$(git status --porcelain |  grep -c '^?? ')
+    hook_com[staged]+="…${untracked_count}"
+fi
+}
+
 precmd() {
   vcs_info
 }
