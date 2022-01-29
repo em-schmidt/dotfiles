3498dbd0cb868beb3f86cb7fb89d3492127fa0f4
eric m. schmidt <eric@spaceba.by>
2022-01-29 11:15:50 -0500

    add remote/local commit diff counts to git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,22 +1,45 @@
 autoload colors && colors
 
 
-# git status prompt bits
+# git status prompt bits see: https://github.com/zsh-users/zsh/blob/master/Misc/vcs_info-examples
+#
+# TODO: further enhacements if needed to selectively run git commands for larer repos or automatic get fetch
+#       see the zsh-users link above around line 336
+#
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:git*' formats " (%b|%c%u)"
-zstyle ':vcs_info:git*' actionformats " (%b|%a%c%u)"
+zstyle ':vcs_info:git*' formats " (%b|%m%c%u)"
+zstyle ':vcs_info:git*' actionformats " (%b|%a%m%c%u)"
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' unstagedstr '✚'
 zstyle ':vcs_info:*' stagedstr '•'
-zstyle ':vcs_info:*+set-message:*' hooks git-untracked
+zstyle ':vcs_info:*+set-message:*' hooks git-untracked git-st
 
 +vi-git-untracked() {
 if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == 'true' ]] && \
-  git status --porcelain | grep -q '^?? ' 2>/dev/null ; then
-  untracked_count=$(git status --porcelain |  grep -c '^?? ')
+  untracked_count=$(git status --porcelain | grep -c '^?? ') ; then
     hook_com[staged]+="…${untracked_count}"
 fi
+}
+
++vi-git-st() {
+  local ahead behind
+  local -a gitstatus
+
+  # Exit early in case the worktree is on a detached HEAD
+  git rev-parse ${hook_com[branch]}@{upstream} >/dev/null 2>&1 || return 0
+
+  local -a ahead_and_behind=(
+    $(git rev-list --left-right --count HEAD...${hook_com[branch]}@{upstream} 2>/dev/null)
+  )
+
+  ahead=${ahead_and_behind[1]}
+  behind=${ahead_and_behind[2]}
+
+  (( $ahead )) && gitstatus+=( "⬆ ${ahead}" )
+  (( $behind )) && gitstatus+=( "⬇ ${behind}" )
+
+  hook_com[misc]+=${(j:/:)gitstatus}
 }
 
 precmd() {
