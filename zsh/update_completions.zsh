8e59925af2b038088752dd36cefd06812adb0303
eric m. schmidt <eric@spaceba.by>
2022-12-28 19:26:58 -0500

    glab autocomplete

--- zsh/update_completions.zsh
+++ zsh/update_completions.zsh
@@ -6,10 +6,12 @@ COMPDIR=~/.dotfiles/zsh/functions
 echo " - kubectl"
 test -x $(which kubectl) && kubectl completion zsh > ${COMPDIR}/_kubectl
 
-
 echo " - flux"
 test -x $(which flux) && flux completion zsh > ${COMPDIR}/_flux
 
 echo " - docker"
 test -x $(which docker) && \
   curl -s https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker > ${COMPDIR}/_docker
+
+echo " - glab"
+test -x $(which glab) && glab completion -s zsh > ${COMPDIR}/_glab
