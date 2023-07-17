f5e2afec9b0c64836b56b47b09a228aa27137d57
eric m. schmidt <eric@spaceba.by>
2023-07-17 16:23:31 -0400

    iterm settings

---
+++ iterm/download_themes.sh
@@ -0,0 +1,12 @@
+
+cd ~/Downloads
+wget https://raw.githubusercontent.com/cdalvaro/github-vscode-theme-iterm/main/GitHub%20Dark%20Default.itermcolors
+wget https://raw.githubusercontent.com/cdalvaro/github-vscode-theme-iterm/main/GitHub%20Dark%20Dimmed.itermcolors
+wget https://raw.githubusercontent.com/cdalvaro/github-vscode-theme-iterm/main/GitHub%20Dark%20High%20Contrast.itermcolors
+wget https://raw.githubusercontent.com/cdalvaro/github-vscode-theme-iterm/main/GitHub%20Light%20Default.itermcolors
+wget https://raw.githubusercontent.com/cdalvaro/github-vscode-theme-iterm/main/GitHub%20Light%20High%20Constrast.itermcolors
+open ~/Downloads/GitHub\ Dark\ Default.itermcolors
+open ~/Downloads/GitHub\ Dark\ Dimmed.itermcolors
+open ~/Downloads/GitHub\ Dark \High\ Contrast.itermcolors
+open ~/Downloads/GitHub\ Light\ Default.itermcolors
+open ~/Downloads/GitHub\ Light\ High\ Contrast.itermcolors
