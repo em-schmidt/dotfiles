b4d1253169e62991b01937151254b3ab3b4c16a1
eric m. schmidt <eric@spaceba.by>
2025-01-22 08:42:32 -0500

    prereq for images in nvin, update git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,12 +1,10 @@
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+alias gdt='git difftool'
 
 alias gc='git commit'
 alias gca='git commit -a'
-alias gsb='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status -sb'
+alias gsb='git switch'
 alias gsm='git switch main'
 alias gfp='git fetch && git pull'
-alias gac='git add -A && git commit -m'
