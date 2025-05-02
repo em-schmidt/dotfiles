bd8f8ca72cbeb48ddfdc746ef7968b1b38c0cca6
eric m. schmidt <eric@spaceba.by>
2025-05-02 19:42:22 -0400

    try out codecompanion

--- nvim/lua/plugins/ai.fnl
+++ nvim/lua/plugins/ai.fnl
@@ -2,9 +2,19 @@
 
 [(u.tx :zbirenbaum/copilot.lua
   {:lazy true
-   :cmd "Copilot"
-   :event "InsertEnter"
-   :opts {}
-   :keys
-     [(u.nlkm :ap "Copilot panel" "panel")]})]
+    :cmd "Copilot"
+    :event "InsertEnter"
+    :opts {}
+    :keys
+      [(u.nlkm :ap "Copilot panel" "panel")]})
 
+ (u.tx :olimorris/codecompanion.nvim
+   {:dependencies
+      ["nvim-lua/plenary.nvim"
+       "nvim-treesitter/nvim-treesitter"]
+    :opts 
+      {:strategies {:chat {:adapter "copilot"}}
+       :opts {:log_level "DEBUG"}}
+    :keys
+      [(u.nlkm :ac "CodeCompanionChat Toggle" "chat")
+       (u.nlkm :aa "CodeCompanionActions" "chat")]})]
--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,3 +1,3 @@
 -- [nfnl] lua/plugins/ai.fnl
 local u = require("util")
-return {u.tx("zbirenbaum/copilot.lua", {lazy = true, cmd = "Copilot", event = "InsertEnter", opts = {}, keys = {u.nlkm("ap", "Copilot panel", "panel")}})}
+return {u.tx("zbirenbaum/copilot.lua", {lazy = true, cmd = "Copilot", event = "InsertEnter", opts = {}, keys = {u.nlkm("ap", "Copilot panel", "panel")}}), u.tx("olimorris/codecompanion.nvim", {dependencies = {"nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter"}, opts = {strategies = {chat = {adapter = "copilot"}}, opts = {log_level = "DEBUG"}}, keys = {u.nlkm("ac", "CodeCompanionChat Toggle", "chat"), u.nlkm("aa", "CodeCompanionActions", "chat")}})}
